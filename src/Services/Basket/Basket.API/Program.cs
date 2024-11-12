4b9af1076f26419cb970a8f836729954ec7f0764
Andrei <7522793+Zyertdox@users.noreply.github.com>
2024-11-12 20:23:29 +0100

    implement basic endpoints

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(opt =>
+{
+    opt.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    opt.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    opt.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
 var app = builder.Build();
+
+app.MapCarter();
 
 app.Run();
