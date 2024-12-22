78e160b99a1177df5cac78174e9ce6e606708998
Andrei <7522793+Zyertdox@users.noreply.github.com>
2024-12-22 23:52:41 +0100

    Add rabbitmq

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+using Ordering.Application.Extensions;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint,
+                                      IFeatureManager featureManager,
+                                      ILogger<OrderCreatedEventHandler> logger): INotificationHandler<OrderCreatedEvent>
+{
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event Handler: OrderCreatedEventHandler");
+
+        if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+        {
+            var orderCreatedIntegrationEvent = domainEvent.Order.ToOrderDto();
+
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
+    }
+}
